e60d3e5aaa967164d608c750a0144182c8f67af6
clarequek <clarequek@hotmail.com>
2025-02-08 00:17:03 +0800

    Added intern experience

--- api/experienceAPI.ts
+++ api/experienceAPI.ts
@@ -1,6 +1,15 @@
 const experienceData = [
     {
         "id": 1,
+        "jobTitle": "Project Management Intern",
+        "company": "Coffee Meets Bagel",
+        "period": "Feb 2025 - Present",
+        "jobDesc": "I conduct user research through interviews to validate feature ideas and gather valuable insights. By analyzing app usage, I identify user pain points, which leads to brainstorming and proposing actionable features to improve the user experience. Additionally, I perform competitor analysis, data analysis using SQL, and market research to inform product decisions and ensure the app remains competitive in the market. This combination of user-centered research and strategic analysis helps drive product development and innovation.",
+        "skills": ["User Research", "Data Analysis", "Competitor Analysis", "Problem-Solving"],
+    },
+
+    {
+        "id": 2,
         "jobTitle": "Temporary Call Centre Tax Advisor",
         "company": "Inland Revenue Authority of Singapore",
         "period": "Feb 2023 - June 2023",
