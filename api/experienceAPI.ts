d151038dadd09632f8d5668022c888e69c321957
clarequekk <clare@coffeemeetsbagel.app>
2025-07-21 21:10:28 +0800

    Debug minor error

--- api/experienceAPI.ts
+++ api/experienceAPI.ts
@@ -5,7 +5,7 @@ const experienceData = [
         "company": "Coffee Meets Bagel",
         "period": "Feb 2025 - Present",
         "jobDesc": "•	Conducted in-depth user interviews to validate feature concepts, uncover user pain points, and inform data-driven product decisions.\n•	Analysed user behaviour through app analytics, identifying key drop-off points and proposing feature optimisations to improve retention.\n•	Own and drive the localisation strategy across both US and worldwide teams, delivering geo-targeted language experiences to improve user engagement and global product adoption.\n•	Collaborated cross-functionally with design and engineering teams to iterate on product features based on research findings.\n•	Contributed to GenAI initiatives by modelling user compatibility scores using machine learning techniques including linear regression, cluster regression, principal component analysis (PCA), and backward stepwise regression.",
-        "skills": ["User Research", "Data Analysis", "Competitor Analysis", "Problem-Solving"],
+        "skills": ["User Research", "Data Analysis", "Competitor Analysis", "Problem-Solving",],
     },
 
     {
--- api/skillsAPI.ts
+++ api/skillsAPI.ts
@@ -6,22 +6,22 @@ const skillsData = [
 
     {
         "id": "Data Tools & Analytics",
-        "skills": ["Tableau Desktop", "R Markdown", "Data Visualisation", "Exploratory Data Analysis", "Regression Modelling (Linear, Cluster, PCA)", "Stepwise Regression", "Mode Analytics"]
+        "skills": ["Tableau Desktop", "R Markdown", "Data Visualisation", "Exploratory Data Analysis", "Regression Modelling (Linear, Cluster, PCA)", "Stepwise Regression", "Mode Analytics",]
     },
 
     {
         "id": "Industry Knowledge",
-        "skills": ["User Interface Design,", "SOLID Design Principles", "Customer Service", "Cross-Functional Collaboration", "Metrics Tracking (e.g., Retention, Engagement)", "User Research & Interviews", "Agile & Sprint Planning", "Backlog Management"]
+        "skills": ["User Interface Design,", "SOLID Design Principles", "Customer Service", "Cross-Functional Collaboration", "Metrics Tracking (e.g., Retention, Engagement)", "User Research & Interviews", "Agile & Sprint Planning", "Backlog Management",]
     },
 
     {
         "id": "Technical Tools & Frameworks",
-        "skills": ["React.js", "React Native", "Firebase", "Git", "GitHub"]
+        "skills": ["React.js", "React Native", "Firebase", "Git", "GitHub",]
     },
 
     {
         "id": "Design",
-        "skills": ["Canva", "Figma", "Prototyping"]
+        "skills": ["Canva", "Figma", "Prototyping",]
     },
 ]
 
